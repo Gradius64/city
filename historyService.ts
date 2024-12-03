81344d7ec8eec375b35cf67e8cb5b194a8ef4f28
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-12-03 14:46:47 -0600

    Add files via upload

---
+++ historyService.ts
@@ -0,0 +1,134 @@
+// TODO: Define a City class with name and id properties
+class City {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+// TODO: Complete the HistoryService class
+class HistoryService {
+  // TODO: Define a read method that reads from the searchHistory.json file
+  private async read() {
+    return await  {
+      flag: 'a+',
+      encoding: 'utf8',
+    };
+  }
+ 
+            
+         
+
+  // Function to read from the searchHistory.json file
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+    
+ 
+
+  
+      
+    
+     
+  
+ 
+   // Output: 10
+  // myObj.readValue = 20; // Error: Cannot assign to 'readValue' because it is a read-only property.
+    
+        
+
+    
+       
+            
+
+
+  
+  
+   
+      
+  
+   
+  // private async read() {}
+  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
+  
+    if (work) {
+        console.log('Successfully written to searchHistory.json');
+    } 
+
+
+ // Define the path to the searchHistory.json file
+ 
+
+
+
+    // Write the JSON data to the file
+    
+
+  
+   
+
+
+
+  
+  
+
+  // private async write(cities: City[]) {}
+  // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
+  
+  getCities(): City[] {
+    try {
+        // Read the file synchronously
+        const data = ('utf8');
+
+        // Parse the JSON data
+        const cities: City[] = JSON.parse(data);
+
+        return cities;
+
+
+
+        class addCity {
+            private cities: string[] = [];
+        
+         addCity(city: string): void {
+            this.cities.push(city)
+         }
+
+         getCities(): string[] {
+            return this.cities;
+        }
+    }
+            
+
+  // async getCities() {}
+  // TODO Define an addCity method that adds a city to the searchHistory.json file
+  class CitySearchHistory {
+    private filePath: string;
+
+    constructor() {
+        this.filePath = ('searchHistory.json');
+    }
+  
+ 
+  // async addCity(city: string) {}
+  // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
+  // async removeCity(id: string) {}
+
+
+
+
+
+
+
+    }
+
